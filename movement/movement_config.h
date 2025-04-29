0f72f0006a30e876debbf33774b8f3e2f40a2b81
Greg <gregoryaevans95@gmail.com>
2025-04-28 21:26:44 -0400

    Set megan's config

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -30,12 +30,14 @@
 const watch_face_t watch_faces[] = {
     simple_clock_face,
     fertility_tracker_face,
-    world_clock_face,
+    wake_face,
     sunrise_sunset_face,
     moon_phase_face,
-    stopwatch_face,
     preferences_face,
     set_time_face,
+    save_load_face,
+    breathing_face,
+    solstice_face,
     thermistor_readout_face,
     voltage_face
 };
@@ -48,7 +50,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 2) // or (0)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 4) // or (0)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
